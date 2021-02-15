1a6406c7179a732a82b85a2d4889f402430a6377
whosegoods-kkwon <kkwon@whosegoods.com>
2021-02-15 09:35:21 +0900

    [UPDATE] FAQ 코드 정리

--- src/containers/faq/FaqAddContainer.js
+++ src/containers/faq/FaqAddContainer.js
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import FaqAddComponent from '../../components/faq/FaqAddComponent';
-import { changeLoginPopup } from '../../modules/Popup';
-import { writeFaq, changeField, initialize } from '../../modules/Write';
-
-const FaqAddContainer = ({ close }) => {
-  const dispatch = useDispatch();
-  const { faqField } = useSelector((state) => state.write);
-  const handleChange = (e) => {
-    const { value, name } = e.target;
-    dispatch(
-      changeField({
-        form: 'faqField',
-        name,
-        value,
-      }),
-    );
-  };
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    const { title, content } = faqField;
-    if (!title || !content) {
-      alert('빈칸이 없어야 합니다.');
-      return;
-    }
-    dispatch(writeFaq({ title, content }));
-    await dispatch(changeLoginPopup(false));
-  };
-  return (
-    <FaqAddComponent
-      onSubmit={onSubmit}
-      onChange={handleChange}
-      close={close}
-    />
-  );
-=======
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { withRouter } from 'react-router-dom';
@@ -105,7 +66,6 @@ const FaqAddContainer = ({ close }) => {
             body={editData.content}
         />
     );
->>>>>>> develop
 };
 
 export default withRouter(FaqAddContainer);
--- src/containers/faq/FaqContainer.js
+++ src/containers/faq/FaqContainer.js
@@ -1,51 +1,5 @@
 import React, { useState, useEffect, shallowEqual } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-<<<<<<< HEAD
-import FaqComponent from '../../components/faq/FaqComponent';
-import { initialize } from '../../modules/Write';
-import { readFaqs } from '../../modules/Faqs';
-import { changeLoginPopup, changeUpdatePopup } from '../../modules/Popup';
-import { initializeFAQ } from '../../modules/Faq';
-
-const FaqContainer = () => {
-  const dispatch = useDispatch();
-  const { Allfaq } = useSelector((state) => state.faqs);
-  const { loginPopup, deletePopup, updatePopup } = useSelector(
-    (state) => state.popup,
-  );
-  const { status } = useSelector((state) => state.write);
-  const { statusUpdate } = useSelector((state) => state.faqReducer);
-  useEffect(() => {
-    dispatch(readFaqs());
-    if (status === 201) {
-      dispatch(readFaqs());
-      dispatch(initialize());
-    } else if (statusUpdate === 200) {
-      dispatch(readFaqs());
-      dispatch(initializeFAQ());
-    }
-  }, [dispatch, status, statusUpdate]);
-  const ClosePopup = async () => {
-    await dispatch(changeLoginPopup(false));
-    dispatch(initializeFAQ());
-  };
-  const OpenPopup = async () => {
-    await dispatch(changeLoginPopup(true));
-  };
-  if (!Allfaq) {
-    return null;
-  }
-  return (
-    <FaqComponent
-      faq={Allfaq}
-      open={OpenPopup}
-      close={ClosePopup}
-      popup={loginPopup}
-      deletePopup={deletePopup}
-      updatePopup={updatePopup}
-    />
-  );
-=======
 
 import { FaqComponent } from 'components';
 
@@ -173,7 +127,6 @@ const FaqContainer = () => {
             />
         </>
     );
->>>>>>> develop
 };
 
 export default FaqContainer;
