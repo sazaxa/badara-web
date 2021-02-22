62ce6ecfeb515bdd78f1a8cfc4758a9645b8d1de
whosegoods-kkwon <kkwon@whosegoods.com>
2021-02-22 17:28:22 +0900

    [UPDATE] admin/order 페이지 waring 수정.

--- src/containers/order/OrderListContainer.js
+++ src/containers/order/OrderListContainer.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import OrderListComponent from 'components/order/OrderListComponent';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
