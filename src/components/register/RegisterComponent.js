2016e2418531be3717f84910ee96ff2a8b0dd66e
whosegoods-kkwon <kkwon@whosegoods.com>
2021-02-23 18:12:10 +0900

    [UPDATE] user/register handling function 구현.

--- src/components/register/RegisterComponent.js
+++ src/components/register/RegisterComponent.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { RegisterWrap, RegisterContent } from '../../styles/RegisterStyles';
 
-const ReigisterComponent = () => {
+const ReigisterComponent = ({ HandleChange, HandleFinish }) => {
     return (
         <RegisterWrap>
             <RegisterContent>
                 <h2>회원가입</h2>
                 <p>회원가입 후 고래타고 서비스를 이용하세요.</p>
                 <form>
-                    <input type="email" name="email" placeholder="*이메일" />
-                    <input type="password" name="password" placeholder="*패스워드" />
-                    <input type="password" name="passwordConfirm" placeholder="*패스워드 확인" />
-                    <input type="text" name="name" placeholder="*이름" />
-                    <input type="number" name="phone" placeholder="*휴대폰 번호" />
-                    <button type="button">회원가입</button>
+                    <input type="email" name="email" placeholder="*이메일" onChange={HandleChange} />
+                    <input type="password" name="password" placeholder="*패스워드" onChange={HandleChange} />
+                    <input
+                        type="password"
+                        name="passwordConfirm"
+                        placeholder="*패스워드 확인"
+                        onChange={HandleChange}
+                    />
+                    <input type="text" name="name" placeholder="*이름" onChange={HandleChange} />
+                    <input type="number" name="phone" placeholder="*휴대폰 번호" onChange={HandleChange} />
+                    <button type="button" onClick={HandleFinish}>
+                        회원가입
+                    </button>
                 </form>
             </RegisterContent>
         </RegisterWrap>
--- src/containers/register/RegisterContainer.js
+++ src/containers/register/RegisterContainer.js
@@ -1,8 +1,47 @@
 import ReigisterComponent from 'components/register/RegisterComponent';
-import React from 'react';
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
 
-const RegisterContainer = () => {
-    return <ReigisterComponent />;
+const RegisterContainer = ({ history }) => {
+    const [registerInfo, setRegisterInfo] = useState({});
+    console.log(registerInfo);
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setRegisterInfo({
+            ...registerInfo,
+            [name]: value,
+        });
+    };
+    const emailCheck = email => {
+        let regex = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+        return email != '' && email != 'undefined' && regex.test(email);
+    };
+    const handleFinish = () => {
+        if (!registerInfo.email) {
+            alert('이메일을 입력하세요.');
+            return;
+        } else if (!emailCheck(registerInfo.email)) {
+            alert('이메일 형식으로 입력하세요.');
+        } else if (!registerInfo.password) {
+            alert('패스워드를 입력하세요.');
+        } else if (registerInfo.password !== registerInfo.passwordConfirm) {
+            alert('패스워드와 패스워드 확인이 틀립니다.');
+            return;
+        } else if (registerInfo.password.length < 8) {
+            alert('패스워드는 8자이상 입력하세요.');
+            return;
+        } else if (!registerInfo.phone) {
+            alert('휴대폰번호를 입력하세요.');
+            return;
+        } else if (!registerInfo.name) {
+            alert('이름을 입력하세요.');
+            return;
+        } else {
+            alert('회원가입 완료.');
+            history.push('/');
+        }
+    };
+    return <ReigisterComponent HandleChange={handleChange} HandleFinish={handleFinish} />;
 };
 
-export default RegisterContainer;
+export default withRouter(RegisterContainer);
