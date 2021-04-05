6515fa70f84f68142b703180895ebc6220265259
whosegoods-kkwon <kkwon@whosegoods.com>
2021-04-05 16:48:37 +0900

    [Fix] 배송대행신청 수취인정보 필수값을 다시 정의하라

--- src/components/user/apply/recipient/Recipient.js
+++ src/components/user/apply/recipient/Recipient.js
@@ -45,156 +45,171 @@ const Recipient = ({ stepIndex, steps }) => {
     const handlePrev = () => {
         dispatch(acitiveStepChange(activeStep - 1));
     };
-    const handleClick = () => {
-        if (
-            recipient.name !== null &&
-            recipient.email !== null &&
-            recipient.country !== null &&
-            recipient.state !== null &&
-            recipient.zipcode !== null &&
-            recipient.countryCode !== null &&
-            recipient.phoneNumber !== null
-        ) {
-            if (recipient.address1 === null && recipient.address2 === null && recipient.address3 === null) {
-                alert('필수 항목을 입력해주세요.');
-                return;
-            } else {
-                dispatch(recipientInsertAction({ recipient: recipient }));
-                dispatch(acitiveStepChange(activeStep + 1));
-            }
-        } else {
-            alert('필수 항목을 입력해주세요.');
-            return;
-        }
+    const handleClick = e => {
+        e.preventDefault();
+        dispatch(recipientInsertAction({ recipient: recipient }));
+        dispatch(acitiveStepChange(activeStep + 1));
+        // if (
+        //     recipient.name !== null &&
+        //     recipient.email !== null &&
+        //     recipient.country !== null &&
+        //     recipient.state !== null &&
+        //     recipient.zipcode !== null &&
+        //     recipient.countryCode !== null &&
+        //     recipient.phoneNumber !== null
+        // ) {
+        //     if (recipient.address1 === null && recipient.address2 === null && recipient.address3 === null) {
+        //         alert('필수 항목을 입력해주세요.');
+        //         return;
+        //     } else {
+        //         dispatch(recipientInsertAction({ recipient: recipient }));
+        //         dispatch(acitiveStepChange(activeStep + 1));
+        //     }
+        // } else {
+        //     alert('필수 항목을 입력해주세요.');
+        //     return;
+        // }
     };
     return (
         <RecipientWrap>
             <h2>수취인 정보</h2>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>이름</th>
-                        <td>
-                            <input
-                                type="text"
-                                name="name"
-                                value={recipient.name ?? undefined}
-                                onChange={e => handleChange(e)}
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>이메일</th>
-                        <td>
-                            <input
-                                type="text"
-                                name="email"
-                                value={recipient.email ?? undefined}
-                                onChange={e => handleChange(e)}
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>국가</th>
-                        <td>
-                            <select
-                                name="country"
-                                value={recipient.country ?? undefined}
-                                onChange={e => handleChange(e)}
-                            >
-                                <option value="">나라선택</option>
-                                {country.list.map(list => (
-                                    <option value={list} key={list}>
-                                        {list}
-                                    </option>
-                                ))}
-                            </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>주소</th>
-                        <td>
-                            <input
-                                type="text"
-                                name="address1"
-                                placeholder="address1"
-                                value={recipient.address1 ?? undefined}
-                                onChange={e => handleChange(e)}
-                                style={{ width: '33%' }}
-                            />
-                            <input
-                                type="text"
-                                name="address2"
-                                placeholder="address2"
-                                value={recipient.address2 ?? undefined}
-                                onChange={e => handleChange(e)}
-                                style={{ width: '33%', marginBottom: '5px' }}
-                            />
-                            <input
-                                type="text"
-                                name="address3"
-                                placeholder="address3"
-                                value={recipient.address3 ?? undefined}
-                                onChange={e => handleChange(e)}
-                                style={{ width: '33%' }}
-                            />
-                            <input
-                                type="text"
-                                name="zipcode"
-                                value={recipient.zipcode ?? undefined}
-                                onChange={e => handleChange(e)}
-                                placeholder="우편번호"
-                                style={{ width: '33%' }}
-                            />
-                            <input
-                                type="text"
-                                name="city"
-                                value={recipient.city ?? undefined}
-                                placeholder="도시"
-                                onChange={e => handleChange(e)}
-                                style={{ width: '33%' }}
-                            />
-                            <input
-                                type="text"
-                                name="state"
-                                placeholder="주/도"
-                                value={recipient.state ?? undefined}
-                                onChange={e => handleChange(e)}
-                                style={{ width: '33%' }}
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>휴대폰번호</th>
-                        <td>
-                            <input
-                                type="text"
-                                placeholder="국가번호"
-                                name="countryCode"
-                                value={recipient.countryCode ?? undefined}
-                                onChange={e => handleChange(e)}
-                                style={{ width: '29.5%' }}
-                            />
-                            <input
-                                type="text"
-                                placeholder="휴대폰번호"
-                                name="phoneNumber"
-                                value={recipient.phoneNumber ?? undefined}
-                                onChange={e => handleChange(e)}
-                                style={{ width: '70%' }}
-                            />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <article className="agreeBox">
-                <Button disabled={stepIndex === 0} onClick={handlePrev}>
-                    이전
-                </Button>
-                <Button variant="contained" color="primary" onClick={handleClick}>
-                    {stepIndex === steps.length - 1 ? '접수하기' : '다음'}
-                </Button>
-            </article>
+            <form onSubmit={e => handleClick(e)}>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>이름</th>
+                            <td>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    value={recipient.name ?? undefined}
+                                    onChange={e => handleChange(e)}
+                                    required
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>이메일</th>
+                            <td>
+                                <input
+                                    type="text"
+                                    name="email"
+                                    value={recipient.email ?? undefined}
+                                    onChange={e => handleChange(e)}
+                                    required
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>국가</th>
+                            <td>
+                                <select
+                                    name="country"
+                                    value={recipient.country ?? undefined}
+                                    onChange={e => handleChange(e)}
+                                    required
+                                >
+                                    <option value="">나라선택</option>
+                                    {country.list.map(list => (
+                                        <option value={list} key={list}>
+                                            {list}
+                                        </option>
+                                    ))}
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>주소</th>
+                            <td>
+                                <input
+                                    type="text"
+                                    name="address1"
+                                    placeholder="address1"
+                                    value={recipient.address1 ?? undefined}
+                                    onChange={e => handleChange(e)}
+                                    style={{ width: '33%' }}
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    name="address2"
+                                    placeholder="address2"
+                                    value={recipient.address2 ?? undefined}
+                                    onChange={e => handleChange(e)}
+                                    style={{ width: '33%', marginBottom: '5px' }}
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    name="address3"
+                                    placeholder="address3"
+                                    value={recipient.address3 ?? undefined}
+                                    onChange={e => handleChange(e)}
+                                    style={{ width: '33%' }}
+                                />
+                                <input
+                                    type="text"
+                                    name="zipcode"
+                                    value={recipient.zipcode ?? undefined}
+                                    onChange={e => handleChange(e)}
+                                    placeholder="우편번호"
+                                    style={{ width: '33%' }}
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    name="city"
+                                    value={recipient.city ?? undefined}
+                                    placeholder="도시"
+                                    onChange={e => handleChange(e)}
+                                    style={{ width: '33%' }}
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    name="state"
+                                    placeholder="주/도"
+                                    value={recipient.state ?? undefined}
+                                    onChange={e => handleChange(e)}
+                                    style={{ width: '33%' }}
+                                    required
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>휴대폰번호</th>
+                            <td>
+                                <input
+                                    type="text"
+                                    placeholder="국가번호"
+                                    name="countryCode"
+                                    value={recipient.countryCode ?? undefined}
+                                    onChange={e => handleChange(e)}
+                                    style={{ width: '29.5%' }}
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="휴대폰번호"
+                                    name="phoneNumber"
+                                    value={recipient.phoneNumber ?? undefined}
+                                    onChange={e => handleChange(e)}
+                                    style={{ width: '70%' }}
+                                    required
+                                />
+                            </td>
+                        </tr>
+                    </tbody>
+                    <article className="agreeBox">
+                        <Button disabled={stepIndex === 0} onClick={handlePrev}>
+                            이전
+                        </Button>
+                        <Button variant="contained" color="primary" type="submit">
+                            {stepIndex === steps.length - 1 ? '접수하기' : '다음'}
+                        </Button>
+                    </article>
+                </table>
+            </form>
         </RecipientWrap>
     );
 };
