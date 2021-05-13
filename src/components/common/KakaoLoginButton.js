9a1cd7b059e4f02522e66b4440c6f81cbc210911
whosegoods-kkwon <kkwon@whosegoods.com>
2021-05-13 10:16:58 +0900

    [Work] 카카오 로그인 진행중...

--- src/components/common/KakaoLoginButton.js
+++ src/components/common/KakaoLoginButton.js
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import KakaoLogin from 'react-kakao-login';
+import KakaoLogout from 'react-kakao-login';
+import { useDispatch } from 'react-redux';
+import { loginAction, registerAction } from 'store/auth';
 
 const KakaoLoginButton = () => {
+    const dispatch = useDispatch();
+    const [kakaoLogin, setKakaoLogin] = useState({
+        email: '',
+        name: '',
+    });
+    const onSuccess = response => {
+        console.log(response.profile.kakao_account.email);
+        setKakaoLogin({
+            email: response.profile.kakao_account.email,
+            name: response.profile.kakao_account.profile.nickname,
+        });
+        console.log(kakaoLogin);
+        dispatch(
+            registerAction({
+                email: response.profile.kakao_account.email,
+                name: response.profile.kakao_account.profile.nickname,
+            })
+        );
+        dispatch(loginAction({ email: response.profile.kakao_account.email, password: null }));
+    };
+
     return (
-        <KakaoLogin
-            token="74a1b4aa5ea36e9a89ad734d26d426d4"
-            buttonText="KaKao"
-            onSuccess={console.log}
-            onFailure={console.error}
-            onLogout={console.info}
-            getProfile={true}
-            style={{
-                width: '100%',
-                color: 'rgb(60, 30, 30)',
-                backgroundColor: 'rgb(255, 235, 0)',
-                border: 'none',
-                cursor: 'pointer',
-            }}
-        >
-            <div className="kakaoBtn">카카오톡 로그인</div>
-        </KakaoLogin>
+        <>
+            <KakaoLogin
+                token="74a1b4aa5ea36e9a89ad734d26d426d4"
+                onSuccess={response => onSuccess(response)}
+                onFailure={console.error}
+                getProfile={true}
+                style={{
+                    width: '100%',
+                    color: 'rgb(60, 30, 30)',
+                    backgroundColor: 'rgb(255, 235, 0)',
+                    border: 'none',
+                    cursor: 'pointer',
+                }}
+            >
+                <div className="kakaoBtn">카카오톡 로그인</div>
+            </KakaoLogin>
+        </>
     );
 };
 
