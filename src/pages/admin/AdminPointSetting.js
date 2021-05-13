a8463771f4667aaf4ca0eb37cbc81fa6055eca68
whosegoods-kkwon <kkwon@whosegoods.com>
2021-05-13 12:51:51 +0900

    [Feat] 포인트 셋팅 추가하라

---
+++ src/pages/admin/AdminPointSetting.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AdminPointSetting = () => {
+    return <div style={{ marginLeft: '300px', padding: '50px 0', height: '100vh' }}>준비중입니다.</div>;
+};
+
+export default AdminPointSetting;
