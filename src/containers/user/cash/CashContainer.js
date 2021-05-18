19ce2c8eaa3d2230a2a6644df160db2d304888d5
whosegoods-kkwon <kkwon@whosegoods.com>
2021-05-18 18:17:21 +0900

    [Fix] 비로그인 상태에서 포인트 확인 페이지 접속시 로그인 안내를 하라

--- src/containers/user/cash/CashContainer.js
+++ src/containers/user/cash/CashContainer.js
@@ -1,10 +1,14 @@
 import { CashComponent } from 'components';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getMemberPointHistoryAction } from 'store/member';
+import { useHistory } from 'react-router';
+import { loginPopupAction } from 'store/auth';
+import { getMemberOrderAction, getMemberPointHistoryAction } from 'store/member';
 
 const CashContainer = () => {
     const dispatch = useDispatch();
+    const history = useHistory();
+    const accessToken = localStorage.getItem('accessToken');
     const { logged, pointHistory } = useSelector(state => ({
         logged: state.member.loggedInfo.logged,
         pointHistory: state.member.loggedInfo.pointHistory,
@@ -15,6 +19,19 @@ const CashContainer = () => {
             dispatch(getMemberPointHistoryAction(logged.id));
         }
     }, [logged]);
+    useEffect(() => {
+        if (accessToken) {
+            if (logged) {
+                dispatch(getMemberOrderAction(logged.id));
+            }
+        } else {
+            alert('로그인이 필요합니다.');
+            // window.location.href = '/';
+            history.push('/');
+            dispatch(loginPopupAction(true));
+        }
+    }, [accessToken, logged]);
+    if (!logged && !accessToken) return null;
     return <CashComponent user={logged} history={pointHistory} />;
 };
 
