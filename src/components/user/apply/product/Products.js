25a29035716d680fd86037bd42b5ce8a058722e7
whosegoods-kkwon <kkwon@whosegoods.com>
2021-05-21 16:52:13 +0900

    [Feat] 상품 등록시 주의 사항을 팝업으로 띄어주고 닫을수 있는 버튼을 3초뒤에 disabled를 해제해줘라

--- src/components/user/apply/product/Products.js
+++ src/components/user/apply/product/Products.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { acitiveStepChange } from 'store/part';
 import { productDataAddAction, productDataRemoveAction } from 'store/apply';
 import Button from '@material-ui/core/Button';
 import Product from './Product';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import Backdrop from '@material-ui/core/Backdrop';
+
+const useStyles = makeStyles(theme => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        // border: '2px solid #000',
+        boxShadow: theme.shadows[2],
+        borderRadius: '5px',
+        padding: theme.spacing(3, 4, 3),
+    },
+    title: {
+        fontSize: '1.2rem',
+        letterSpacing: '-1.5px',
+        color: 'red',
+        marginBottom: '1rem',
+    },
+    description: {
+        marginBottom: '0.5rem',
+        letterSpacing: '-1.5px',
+    },
+}));
 
 const Products = ({ stepIndex, steps }) => {
+    const [informainPopup, setInfomatinPopup] = useState(true);
+    const [btnActive, setBtnAction] = useState(false);
+    const [seconds, setSeconds] = useState(3);
+    const classes = useStyles();
     const dispatch = useDispatch();
     const { activeStep, products: applyProduct } = useSelector(
         state => ({
@@ -20,6 +52,21 @@ const Products = ({ stepIndex, steps }) => {
         quantity: null,
         price: null,
     };
+
+    useEffect(() => {
+        setTimeout(() => {
+            setBtnAction(true);
+        }, 3000);
+    }, []);
+
+    useEffect(() => {
+        const countdown = setInterval(() => {
+            if (parseInt(seconds) > 0) {
+                setSeconds(parseInt(seconds) - 1);
+            }
+        }, 1000);
+        return () => clearInterval(countdown);
+    }, [seconds]);
 
     const handlePrev = () => {
         dispatch(acitiveStepChange(activeStep - 1));
@@ -43,6 +90,9 @@ const Products = ({ stepIndex, steps }) => {
             }
         }
     };
+    const handleInfomationPopup = () => {
+        setInfomatinPopup(!informainPopup);
+    };
 
     const ProductAdd = () => {
         dispatch(productDataAddAction({ product: defaultProduct }));
@@ -52,8 +102,43 @@ const Products = ({ stepIndex, steps }) => {
     const ProductRemove = index => {
         dispatch(productDataRemoveAction({ index: index }));
     };
+
     return (
         <>
+            <Modal
+                aria-labelledby="spring-modal-title"
+                aria-describedby="spring-modal-description"
+                className={classes.modal}
+                open={informainPopup}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <div className={classes.paper}>
+                    <h2 className={classes.title}>※ 바다라 배송금지 품목</h2>
+                    <p className={classes.description} style={{ marginBottom: '1rem' }}>
+                        석면, 총화기류, 모피, 보석류, 동/식물, 국가별 금지 품목
+                    </p>
+                    <h2 className={classes.title}>※ 식품 발송 주의 사항</h2>
+                    <p className={classes.description}>
+                        - 식품에 동물 성분이 포함된 것이 확인되면 현지 세관에서 반송할 수 있습니다. 이 경우 반송비가
+                        청구될 수 있으며, 상품 재발송을 위해서는 추가 비용을 결제해주셔야 합니다.
+                    </p>
+                    <p className={classes.description}>- 국가별로 발송 가능한 식품은 상이합니다.</p>
+                    <div className="btnWrap">
+                        <button
+                            type="button"
+                            onClick={() => handleInfomationPopup()}
+                            disabled={btnActive ? false : true}
+                        >
+                            닫기
+                        </button>
+                        <p>(약 {seconds}초 뒤 닫기가 가능합니다.)</p>
+                    </div>
+                </div>
+            </Modal>
             <form onSubmit={handleClick}>
                 {applyProduct?.map((v, index) => (
                     <Product
