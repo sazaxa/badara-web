63fead645674cc4ff55e6d3fc2ea0c1c21fc0108
whosegoods-kkwon <kkwon@whosegoods.com>
2021-05-27 16:37:32 +0900

    [Feat] 이벤트 팝업을 추가하라

--- src/components/user/main/MainContentComponet.js
+++ src/components/user/main/MainContentComponet.js
@@ -2,18 +2,22 @@ import { CalculatorContainer } from 'containers';
 import React from 'react';
 import { Responsive } from 'styles/CommonStyles';
 import { MainWrap } from 'styles/MainPageStyles';
+import EventPopup from './EventPopup';
 import MainBanner from './MainBanner';
 import Tracking from './Tracking';
 
 const MainContentComponent = () => {
     return (
-        <Responsive>
-            <MainWrap>
-                <MainBanner />
-                <CalculatorContainer />
-                <Tracking />
-            </MainWrap>
-        </Responsive>
+        <>
+            <EventPopup />
+            <Responsive>
+                <MainWrap>
+                    <MainBanner />
+                    <CalculatorContainer />
+                    <Tracking />
+                </MainWrap>
+            </Responsive>
+        </>
     );
 };
 
